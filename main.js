c26bc91c31348aea2fcc7710f97bfd05ed89b2a2
tran anh vu <eyelight93@gmail.com>
2017-05-15 17:51:37 +0700

    final change

--- main.js
+++ main.js
@@ -3,12 +3,12 @@ $(document).ready(function() {
   var myTimer = setInterval(function() {
 
     swal({
-      title: "Het, get a life!",
+      title: "Hey, get a life!",
       text: "You have been here for 5 minutes, stop scrolling",
       type: "warning",
       showCancelButton: true,
       confirmButtonColor: "#DD6B55",
-      confirmButtonText: "Okay",
+      confirmButtonText: "OK",
       cancelButtonText: "Just 5 more minutes..",
       closeOnConfirm: false,
     },
